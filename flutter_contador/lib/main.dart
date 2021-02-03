696487071975b0b2b8e774a08298ad54bae62e94
yagobmarques <yagobsmarques@gmail.com>
2021-02-03 12:47:46 -0300

    contador

---
+++ flutter_contador/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(title: "Contador de pessoas", home: Home())); //Material App
+}
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  int _pessoa = 0;
+  String _mensagem = "Pode entrar!";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _pessoa += delta;
+      if (_pessoa >= 20) {
+        _mensagem = "Lotado, não pode entrar.";
+        _pessoa = 20;
+      } else if (_pessoa >= 0 && _pessoa < 20) {
+        _mensagem = "Pode entrar!";
+      }
+      else{
+        _pessoa = 0;
+        _mensagem = "Número negativo";
+      }
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "img/215.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_pessoa",
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold, decoration: TextDecoration.none),
+            ), //text
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              _mensagem,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30,
+                  decoration: TextDecoration.none),
+            ) //text
+          ], //widget
+        ) //Column
+      ],
+    );
+  }
+}
